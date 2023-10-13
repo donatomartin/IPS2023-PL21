3c32e3f1d9f9bd9ea90c269fa61857dd2169f408
liitt <liitt@Angelpc.ANGELDESKTOP>
2023-10-13 14:41:33 +0200

    Creacion de la clase Reserva 21913

--- src/main/java/ips2023pl21/model/Reservas.java
+++ src/main/java/ips2023pl21/model/Reservas.java
@@ -2,4 +2,21 @@ package ips2023pl21.model;
 
 public class Reservas {
 
+	//Atributos de la propia reserva
+		private String nombreUsuario;
+		private String cuentaBancaria;
+		private int precioReserva;
+		private String fechaReserva;
+		private int horaReservaEntrada;
+		private int minutoReservaEntrada;
+		private int horaReservaSalida;
+		private int minutoReservaSalida;
+		
+		//Atributos almacenados para el historial de ventas
+		private String fechaVenta;
+		private int horaVenta;
+		private int minutoVenta;
+
+		
+		public Reservas() {}
 }
