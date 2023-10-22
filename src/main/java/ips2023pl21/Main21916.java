0b27427c1fb89f1ad1807b72095ec5b93f0d2bd2
yagon <yagon@192.168.1.198>
2023-10-22 20:30:55 +0200

    restructuracion 21916

--- src/main/java/ips2023pl21/Main21916.java
+++ src/main/java/ips2023pl21/Main21916.java
@@ -5,15 +5,7 @@ import ips2023pl21.ui.VentanaTienda;
 public class Main21916 {
 
 	public static void main(String[] args) {
-//		Database db = new Database();
-//		db.createDatabase(false);
-//		db.loadDatabase();
-//		List<EmpleadoDeportivo> lista = db.executeQueryPojo(EmpleadoDeportivo.class, 
-//				"select * from empleadodeportivo");
-//		
-//		for(EmpleadoDeportivo ed : lista) {
-//			System.out.println(ed.getNombre());
-//		}
+
 		VentanaTienda vt = new VentanaTienda();
 	}
 
