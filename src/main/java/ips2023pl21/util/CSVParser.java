162c03f8dee08307f95d5df58691fca541f3d65f
UO287746 <UO287746@EII-L06-09.aulasuo.uniovi.es>
2023-11-16 09:55:33 +0100

    añadidos botones de carga de partidos

--- src/main/java/ips2023pl21/util/CSVParser.java
+++ src/main/java/ips2023pl21/util/CSVParser.java
@@ -1,8 +1,15 @@
 package ips2023pl21.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ips2023pl21.model.equipos.Partido;
+
 public class CSVParser {
 
-		public CSVParser(String archivo) {
+		public static List<Partido> parsePartidosCSV(String archivo){
+			ArrayList<Partido> partidos = new ArrayList<>();
 			
+			return partidos;
 		}
 }
